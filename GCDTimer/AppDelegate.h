71292e6866ba6ee8a554cb535b55f2c295ed1b98
hesfirst <962727696@qq.com>
2016-08-29 15:57:48 +0800

    Initial Commit

---
+++ GCDTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCDTimer
+//
+//  Created by King on 16/8/29.
+//  Copyright © 2016年 King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCDTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCDTimer
+//
+//  Created by King on 16/8/29.
+//  Copyright © 2016年 King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
